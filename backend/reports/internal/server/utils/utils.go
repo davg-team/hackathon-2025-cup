af34eac83867ff1ad630b022cd7c8d394c25f617
glebkasilov <kasilovgleb7@gmail.com>
2025-04-22 21:39:10 +0500

    add banch loading results

--- backend/reports/internal/server/utils/utils.go
+++ backend/reports/internal/server/utils/utils.go
@@ -4,19 +4,20 @@ import (
 	"errors"
 	"net/http"
 
+	customerrors "github.com/davg/pkg/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func HandleError(err error, ctx *gin.Context) {
-	if errors.Is(err, errors.New("validation error")) {
+	if errors.Is(err, customerrors.ErrBadRequest) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if errors.Is(err, errors.New("victory not found")) {
+	if errors.Is(err, customerrors.ErrNotFound) {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
-	if errors.Is(err, errors.New("internal error")) {
+	if errors.Is(err, customerrors.ErrInternal) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
