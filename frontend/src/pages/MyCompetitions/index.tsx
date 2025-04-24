0d4ed0b9757d30480378e6d81cc8b61113c17218
MrBallonvas <vasilisrusgame@gmail.com>
2025-04-24 21:30:38 +0500

    my competitions

--- frontend/src/pages/MyCompetitions/index.tsx
+++ frontend/src/pages/MyCompetitions/index.tsx
@@ -12,7 +12,7 @@ import {
 
 import PageConstr from "features/components/PageConstr";
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { getRoleFromToken /*getTimeAsDayMonthYear*/ } from "shared/tools";
 import {
   disciplinesObject as disciplines,
@@ -70,7 +70,7 @@ export function MyCompetitionsMainContent() {
   function getStatus(app: any) {
     const now = new Date();
     const start = new Date(app.start_date);
-    const end = new Date(app.end_date);
+    const end = new Date(app.event_date);
     if (now < start) return "upcoming";
     if (now >= start && now <= end) return "active";
     return "ended";
@@ -133,56 +133,63 @@ export function MyCompetitionsMainContent() {
             <Loader />
           ) : filteredApps.length > 0 ? (
             filteredApps.map((app) => (
-              <Card
-                key={app.application_id}
-                view="filled"
-                maxWidth={"340px"}
-                width={"max"}
-                className={spacing({ p: 3 })}
+              <Link
+                style={{ color: "inherit", textDecoration: "none" }}
+                to={"/competitions/" + app.event_id}
               >
-                <Flex
-                  direction="column"
-                  height="100%"
-                  justifyContent="space-between"
+                <Card
+                  key={app.application_id}
+                  view="filled"
+                  maxWidth={"340px"}
+                  width={"max"}
+                  className={spacing({ p: 3 })}
                 >
-                  <Flex direction="column">
-                    <img src="" height="196" width="320" />
-                    <br />
-                    <Text variant="display-2" style={{ width: "250px" }}>
-                      {app.team_name}
-                    </Text>
-                    <br />
-                    <Flex gap="2" wrap="wrap" alignItems="center">
-                      <Label size="xs" theme="warning">
-                        {disciplines["algorithms"]}
-                      </Label>
-                      <Label size="xs" theme="info">
-                        {types["interregional"]}
-                      </Label>
-                      <Label size="xs" theme="success">
-                        {app.members.length}
-                      </Label>
+                  <Flex
+                    direction="column"
+                    height="100%"
+                    justifyContent="space-between"
+                  >
+                    <Flex direction="column">
+                      <img src="" height="196" width="320" />
+                      <br />
+                      <Text variant="display-2" style={{ width: "250px" }}>
+                        {app.team_name}
+                      </Text>
+                      <br />
+                      <Flex gap="2" wrap="wrap" alignItems="center">
+                        <Label size="xs" theme="warning">
+                          {disciplines["algorithms"]}
+                        </Label>
+                        <Label size="xs" theme="info">
+                          {types["interregional"]}
+                        </Label>
+                        <Label size="xs" theme="success">
+                          {app.members.length}
+                        </Label>
+                      </Flex>
                     </Flex>
-                  </Flex>
-                  <br />
-                  <Flex gap="2" wrap="wrap" justifyContent="initial">
-                    <Flex alignItems="center" gap="2">
-                      {/*<Icon data={Calendar} />
+                    <br />
+                    <Flex gap="2" wrap="wrap" justifyContent="initial">
+                      <Flex alignItems="center" gap="2">
+                        {/*<Icon data={Calendar} />
                       <Text variant="body-2">
                         {getTimeAsDayMonthYear(app.start_date)} -{" "}
                         {getTimeAsDayMonthYear(app.end_date)}
                       </Text>*/}
+                      </Flex>
+                      <Button>
+                        {app.application_status === "pending"
+                          ? "На модерации"
+                          : app.application_status === "team"
+                            ? "Набор команды"
+                            : getStatus(app) === "active"
+                              ? "Одобрено"
+                              : "Завершился"}
+                      </Button>
                     </Flex>
-                    <Button>
-                      {app.application_status === "pending"
-                        ? "На модерации"
-                        : getStatus(app) === "active"
-                          ? "Одобрено"
-                          : "Завершился"}
-                    </Button>
                   </Flex>
-                </Flex>
-              </Card>
+                </Card>
+              </Link>
             ))
           ) : (
             <>В этой категории нет заявок</>
